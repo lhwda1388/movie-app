0217a98a703d373620a080ff0601f1dc4c62b644
LEE HYUN WOO <lhwda1388@naver.com>
2020-05-19 23:31:03 +0900

    add router

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+function About(props) {
+  console.log(props)
+  return (
+    <div className="about__container">
+      <span>"^^”</span>
+      <span>− Lhw, 1988</span>
+    </div>
+  )
+}
+
+export default About
